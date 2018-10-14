2117e1922b9165a2e30cb57a6eba6dfeb49a86d7
Izidro Queiroz <izidro.queiroz@gmail.com>
2018-10-13 22:40:52 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -12,8 +12,4 @@ public interface EmailService {
 		
 	void sendEmail(SimpleMailMessage msg);
 	
-	void sendOrderConfirmationHtmlEmail(Pedido obj);
-	
-	void sendHtmlEmail(MimeMessage msg);
-	
 }
--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -68,7 +68,7 @@ public class PedidoService {
 			
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationHtmlEmail(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 
