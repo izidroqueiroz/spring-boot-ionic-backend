13959819b41d090032cd286aabbb0b49df738aa5
Izidro Queiroz <izidro.queiroz@gmail.com>
2018-12-16 11:35:40 -0200

    teste

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -203,12 +203,12 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "izidro.queiroz@gmail.com", "36378912377", TipoCliente.PESSOAFISICA,
-				pe.encode("123"));
+				pe.encode("will2709"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		
 		Cliente cli2 = new Cliente(null, "Ana Costa", "izidro.queiroz@outlook.com", "36399590515", TipoCliente.PESSOAFISICA,
-				pe.encode("123"));
+				pe.encode("will2709"));
 		cli2.getTelefones().addAll(Arrays.asList("26763323", "93567393"));
 		cli2.addPerfil(Perfil.ADMIN);
 
